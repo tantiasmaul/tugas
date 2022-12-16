69b34a089c17ab05cb0df995a050f92e07e216e1
tantiasmaul <116443956+tantiasmaul@users.noreply.github.com>
2022-12-16 20:53:18 +0700

    Create koneksi.php

---
+++ config/koneksi.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('DB','dbrest');
+define('PASS','');
+$conn = new mysqli(HOST,USER,PASS,DB) or die('Connetion error to the database');
